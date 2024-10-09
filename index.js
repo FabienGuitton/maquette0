f1a3fd4f535f0d579c94c2d4241c46427609a256
FabienGuitton <fabienguittonfr@gmail.com>
2024-10-09 22:44:50 +0200

    Modification page 1 (Ajout JS)

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-let bouton_recrutement = document.getElementById('menu_changement_recrutement');
-let bouton_interim = document.getElementById('menu_changement_interim');
-let bouton_performance = document.getElementById('menu_changement_performance');
-let bouton_transformation = document.getElementById('menu_changement_transformation');
+let bouton_recrutement = document.getElementById('btnRecrutement');
+let bouton_interim = document.getElementById('btnInterim');
+let bouton_performance = document.getElementById('btnPerformance');
+let bouton_transformation = document.getElementById('btnTransformation');
 let backColor = document.getElementById('bloc_1');
 
-bouton_recrutement.addEventListener('click', function(){
-    backColor.style.backgroundColor = "#2ca0ab";
-    bouton_recrutement.style.backgroundColor = "#2ca0ab";
-});
+let btn_sliders = document.getElementsByClassName('text-btn-slider');
 
-bouton_interim.addEventListener('click', function(){
-    backColor.style.backgroundColor = "#b54340";
-    bouton_interim.style.backgroundColor = "#b54340";
-});
-
-bouton_performance.addEventListener('click', function(){
-    backColor.style.backgroundColor = "#dba569";
-    bouton_performance.style.backgroundColor = "#dba569";
-});
-
-bouton_transformation.addEventListener('click', function(){
-    backColor.style.backgroundColor = "#005a3d";
-    bouton_transformation.style.backgroundColor = "#005a3d";
-});
+for(let i = 0; i < btn_sliders.length; i++) {
+    /* Je peux créer une variable : let mybtn = btn_sliders[i]; pour le mettre partout et plus lisible */
+    btn_sliders[i].addEventListener("click", function() {
+        let recupColor = btn_sliders[i].getAttribute("color-background");
+        console.log("couleur " + btn_sliders[i].style.backgroundColor)
+        if (btn_sliders[i].style.backgroundColor == '') {
+            backColor.style.backgroundColor = recupColor;
+            btn_sliders[i].style.backgroundColor = recupColor;
+        } else {
+            backColor.style.backgroundColor = '#5593b3';
+            btn_sliders[i].style.backgroundColor = '';
+        }
+    
+        console.log("test " + i);
+    })
+}
